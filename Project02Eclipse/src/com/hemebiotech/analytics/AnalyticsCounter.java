f38a1d7c33e61c26033bf96fddaf0d9a5bb39389
Emmanuel Edjam <emmanueledjametchaki@gmail.com>
2021-09-12 20:03:29 +0000

    implementation of the data reading method

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,13 +1,35 @@
 package com.hemebiotech.analytics;
 
+import java.io.IOException;
+import java.util.List;
+
 public class AnalyticsCounter {
-	
+	/**
+	 * Path of the file containing the data to analyze
+	 * 
+	 * @see ISymptomReader
+	 * @see ReadSymptomDataFromFile
+	 */
 	private String filePatch;
-	
-	public void start(String filePatch) {
-		
-		this.filePatch=filePatch;
-		
+	/**
+   	 * Initialization of the list which will store the data of the read file
+	 * 
+	 * @see ISymptomReader
+	 */
+	private List<String> symptomsList;
+
+	/**
+	 * An implementation that groups together the functions that will be executed by the program
+	 * 
+	 * @param filePatch It is the source file that is to be analyzed
+	 * 
+	 * 
+	 * @throws IOException
+	 */
+	public void start(String filePatch) throws IOException {
+
+		this.filePatch = filePatch;
+
 		reading();
 		counting();
 		sorting();
@@ -15,23 +37,28 @@ public class AnalyticsCounter {
 	}
 
 	private void reading() {
-		// TODO Auto-generated method stub
-		
+		/**
+		 * method which reads the source file through a previous implementation
+		 * 
+		 * @see ReadSymptomDataFromFile
+		 */
+		ReadSymptomDataFromFile readSymptomDataFromFile = new ReadSymptomDataFromFile(filePatch);
+		symptomsList = readSymptomDataFromFile.GetSymptoms();
 	}
 
 	private void counting() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void sorting() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	private void writing() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 }
